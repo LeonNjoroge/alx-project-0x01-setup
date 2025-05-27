90fddd7a8bb9104e3e4fa5659b0e8ccb2b3d3e62
LeonNjoroge <leonmuraguri@gmail.com>
2025-05-27 12:10:21 +0300

    Feat: Complete Proj-0 Task-1

---
+++ alx-project-0x01/components/common/PostCard.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const PostCard: React.FC = () => {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default PostCard;
---
+++ alx-project-0x01/pages/posts/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Posts: React.FC = () => {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default Posts;
