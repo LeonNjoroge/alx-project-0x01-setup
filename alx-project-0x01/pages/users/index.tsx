3f71cde5ec2f3120c1f80ee6901d3e1c1099da83
LeonNjoroge <leonmuraguri@gmail.com>
2025-05-27 12:15:15 +0300

    Feat: Complete Proj-0 Task-1

--- alx-project-0x01/pages/users/index.tsx
+++ alx-project-0x01/pages/users/index.tsx
@@ -1,9 +1,11 @@
 import React from "react";
+import Header from "@/components/layout/Header";
+
 
 const Users: React.FC = () => {
     return (
         <div>
-
+            <Header />
         </div>
     )
 }
